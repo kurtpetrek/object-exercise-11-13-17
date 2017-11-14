9a5f0e36cedfccd6bdb3844f3fe73e3a4b7e98f3
Kurt Petrek <kurtpetrek@gmail.com>
2017-11-13 20:15:39 -0500

    starting repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Object Exercise
+
+This is an exercise in representing everyday things as an object. In this
+example I create a desk object.
---
+++ js/script.js
@@ -0,0 +1,12 @@
+var car = {
+  make: "Toyota",
+  model: "Camry",
+  year: 1988,
+  wheels: 4,
+  currentlyRuns: true,
+  engine: {
+    pistons: 4,
+    hasOil: true,
+    fluids: ["transmission fluid", "Brake fluid"]
+  }
+};
